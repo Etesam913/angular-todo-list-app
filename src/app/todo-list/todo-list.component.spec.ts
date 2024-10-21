3c3d79c045effa2d56d2d2a1cee3748aae52ba45
etesam <etesamansari13@gmail.com>
2024-10-21 19:16:43 -0400

    :white_check_mark: Added delete and add todo test

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,4 +1,8 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from "@angular/core/testing";
 
 import { TodoListComponent } from "./todo-list.component";
 import { provideRouter } from "@angular/router";
@@ -10,7 +14,10 @@ describe("TodoListComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TodoListComponent],
-      providers: [provideRouter([])],
+      providers: [
+        provideRouter([]),
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TodoListComponent);
@@ -25,6 +32,67 @@ describe("TodoListComponent", () => {
   it("should have todos", () => {
     const todoListContainer: HTMLElement = fixture.nativeElement;
     const todoItems = todoListContainer.querySelectorAll(".todo-item");
-    expect(todoItems.length === 2);
+    expect(todoItems.length).toBe(2);
   });
+
+  it("should add a todo", async () => {
+    const todoListContainer: HTMLElement = fixture.nativeElement;
+
+    const titleInput: HTMLInputElement | null =
+      todoListContainer.querySelector("#new-todo-title");
+
+    if (!titleInput) {
+      fail("title input not found");
+      return;
+    }
+
+    titleInput.value = "the new todo title to add";
+    titleInput.dispatchEvent(new Event("input"));
+
+    // Wait for Angular to update the display binding through the title pipe
+    await fixture.whenStable();
+
+    const descriptionTextArea: HTMLTextAreaElement | null =
+      todoListContainer.querySelector("#new-todo-description");
+
+    if (!descriptionTextArea) {
+      fail("text area not found");
+      return;
+    }
+
+    descriptionTextArea.value = "the new todo description to add";
+    descriptionTextArea.dispatchEvent(new Event("input"));
+
+    await fixture.whenStable();
+
+    const newTodoSubmitButton: HTMLButtonElement | null =
+      todoListContainer.querySelector("#new-todo-submit");
+
+    if (!newTodoSubmitButton) {
+      fail("submit button not found");
+      return;
+    }
+    newTodoSubmitButton.click();
+    await fixture.whenStable();
+
+    const todoItems = todoListContainer.querySelectorAll(".todo-item");
+    expect(todoItems.length).toBe(3);
+  });
+
+  it("should delete todos", async () => {
+    const todoListContainer: HTMLElement = fixture.nativeElement;
+
+    const deleteTodoButtons: NodeListOf<HTMLButtonElement> =
+      todoListContainer.querySelectorAll("[data-testid='delete-todo-button']");
+
+    for (let i = 0; i < deleteTodoButtons.length; i++) {
+      deleteTodoButtons[i].click();
+      await fixture.whenStable();
+    }
+
+    const todoItems = todoListContainer.querySelectorAll(".todo-item");
+    expect(todoItems.length).toBe(0);
+  });
+
+  it("should strikethrough todo", async () => {});
 });
